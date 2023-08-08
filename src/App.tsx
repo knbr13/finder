91680fea759dc1455c948412230d5a70977f5d04
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-08-08 12:21:09 +0300

    create Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import { useEffect } from "react";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<div>login</div>} />
+          <Route path="/login" element={<Login />} />
           <Route path="/artist" element={<div>artist</div>} />
           <Route path="/albums" element={<div>albums</div>} />
           <Route path="*" element={<Redirector />} />
