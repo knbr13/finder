05fe9b5004316bbc5e886fc79330675382c285fe
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-08-08 20:01:07 +0300

    use ArtistPage as an element for /artist path

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import { useEffect } from "react";
 import Login from "./pages/Login";
-import Artist from "./pages/ArtistPage";
 import Album from "./pages/Albums";
+import ArtistPage from "./pages/ArtistPage";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/artist" element={<Artist />} />
+          <Route path="/artist" element={<ArtistPage />} />
           <Route path="/album/:artistId" element={<Album />} />
           <Route path="*" element={<Redirector />} />
         </Routes>
