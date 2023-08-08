d4679d40b40eb96f9b16b4f9946565dbb4a3a95a
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-08-08 15:16:12 +0300

    update the size of Artists component

--- src/components/Artists.tsx
+++ src/components/Artists.tsx
@@ -8,7 +8,7 @@ interface ArtistsProps {
 
 const Artists: React.FC<ArtistsProps> = ({ artists, onArtistClick }) => {
   return (
-    <div className="grid bg-red-300 p-10 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="grid place-content-center p-10 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
       {artists.map((artist) => (
         <Artist
           key={artist.id}
