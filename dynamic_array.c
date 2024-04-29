80f9c725cf358a14d106204b0a081fc020f4144b
knbr13 <abdullahalaadine63@gmail.com>
2024-04-29 18:02:54 +0300

    implement create_dynamic_array func

---
+++ dynamic_array.c
@@ -0,0 +1,14 @@
+#include "dynamic_array.h"
+
+#include <stdlib.h>
+
+DynamicArray *create_dynamic_array(size_t initial_capacity) {
+    DynamicArray *arr = calloc(1, sizeof(DynamicArray));
+    char *underlying_arr = calloc(initial_capacity, sizeof(char));
+
+    arr->size = initial_capacity;
+    arr->capacity = initial_capacity;
+    arr->data = underlying_arr;
+
+    return arr;
+}
