37cef9f9816c28003f9d525d32597285a8cd610a
knbr13 <abdullahalaadine63@gmail.com>
2024-04-30 05:49:02 +0300

    update dynamic_array_pop prototype

--- dynamic_array.h
+++ dynamic_array.h
@@ -14,7 +14,7 @@ void destroy_dynamic_array(DynamicArray *array);
 void dynamic_array_resize(DynamicArray *array, size_t new_capacity);
 void dynamic_array_append(DynamicArray *array, char value);
 void dynamic_array_set_at(DynamicArray *array, char index, char value);
-void dynamic_array_remove(DynamicArray *array, char index);
+char dynamic_array_pop(DynamicArray *array);
 void dynamic_array_print(DynamicArray *array);
 unsigned int dynamic_array_size(DynamicArray *array);
 unsigned int dynamic_array_capacity(DynamicArray *array);
