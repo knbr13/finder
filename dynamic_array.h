ce2146a753c75193e55ba9944ac2091257c10a6e
knbr13 <abdullahalaadine63@gmail.com>
2024-04-30 05:54:14 +0300

    add dynamic_array_get_at prototype

--- dynamic_array.h
+++ dynamic_array.h
@@ -13,9 +13,9 @@ DynamicArray *create_dynamic_array(size_t initial_capacity);
 void destroy_dynamic_array(DynamicArray *array);
 void dynamic_array_resize(DynamicArray *array, size_t new_capacity);
 void dynamic_array_append(DynamicArray *array, char value);
+char dynamic_array_get_at(DynamicArray *array, size_t index);
 void dynamic_array_set_at(DynamicArray *array, char index, char value);
 char dynamic_array_pop(DynamicArray *array);
-void dynamic_array_print(DynamicArray *array);
 size_t dynamic_array_size(DynamicArray *array);
 size_t dynamic_array_capacity(DynamicArray *array);
 
