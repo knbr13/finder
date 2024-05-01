286766dd3e2c559e72373971fe48fbdfd1273836
knbr13 <abdullahalaadine63@gmail.com>
2024-05-01 05:58:41 +0300

    create manual read_line func

--- main.c
+++ main.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
 
+#include "dynamic_array.h"
+
+unsigned int read_line(FILE *stream, DynamicArray *arr) {
+    char c = getc(stream);
+    if (c == 'q') {
+        return 0;
+    }
+
+    unsigned int count = 0;
+
+    for (; c != '\n' && c != EOF; c = getc(stream), count++) {
+        dynamic_array_append(arr, c);
+    }
+
+    return count;
+}
+
 int main() {
     printf("Super Calculator: \n");
 
     for (;;) {
         printf("> ");
-        char *line = NULL;
-        size_t len = 0;
-        getline(&line, &len, stdin);
-        if (line[0] == 'q') {
+        DynamicArray *arr = create_dynamic_array(24);
+        unsigned int r = read_line(stdin, arr);
+        if (r == 0) {
             break;
         }
-        printf("%s\n", line);
+        for (int i = 0; i < arr->size; i++) {
+            printf("%c", dynamic_array_get_at(arr, i));
+        }
+        printf("\n");
     }
 
     return 0;
