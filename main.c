ea9d2de5d7ae3ac3ed492f2b5a8d8eef532e8976
knbr13 <abdullahalaadine63@gmail.com>
2024-05-02 20:16:47 +0300

    use LineResult as a return type

--- main.c
+++ main.c
@@ -1,46 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *readline(FILE *stream) {
+typedef struct {
+    char *line;
+    int END_OF_FILE;
+} LineResult;
+
+LineResult readline(FILE *stream) {
     int offset = 0, buf_size = 4, c;
 
     char *buf = malloc(buf_size * sizeof(char));
     if (buf == NULL) {
-        return NULL;
+        return (LineResult){.line = NULL, .END_OF_FILE = 0};
     }
 
-    while ((c = getc(stream)) != '\n' && c != EOF) {
+    while ((c = getc(stream)) != '\n') {
+        if (c == EOF) {
+            free(buf);
+            return (LineResult){.line = NULL, .END_OF_FILE = 1};
+        }
         if (offset == buf_size - 1) {
             char *new_buf = realloc(buf, (buf_size *= 2));
             if (new_buf == NULL) {
                 free(buf);
-                return NULL;
+                return (LineResult){.line = NULL, .END_OF_FILE = 0};
             }
             buf = new_buf;
         }
         buf[offset++] = c;
     }
 
-    if (c == EOF && offset == 0) {
-        free(buf);
-        return NULL;
-    }
-
     if (offset < buf_size - 1) {
         char *new_buf = realloc(buf, offset + 1);
         if (new_buf == NULL) {
-            return NULL;
+            return (LineResult){.line = NULL, .END_OF_FILE = 0};
         }
         buf = new_buf;
     }
 
     buf[offset] = '\0';
-    return buf;
+    return (LineResult){.line = buf, .END_OF_FILE = 0};
 }
 
 int main() {
     printf("enter your name: ");
-    char *line = readline(stdin);
-    printf("hello, %s\n", line);
+    LineResult res = readline(stdin);
+    printf("'%s'\n", res.line);
     return 0;
 }
